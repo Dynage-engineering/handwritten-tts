d9717c4f12311544172a002b6c941d71678e353f
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-02-18 15:59:07 +0100

    Add language selection for text-to-speech conversion

--- api/hand_text.py
+++ api/hand_text.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import pytesseract
 from gtts import gTTS
+import gtts
 import os
 from PIL import Image
 import numpy as np
@@ -35,22 +36,27 @@ def get_hand_text_from_canvas():
 
         # Use pytesseract to extract text
         text = pytesseract.image_to_string(img)
-
+        languages = gtts.lang.tts_langs()
+        language = st.selectbox("Select language", list(languages.values()))
+        st.write("Selected language:", language)
         st.write("Recognized text:", text)
 
         # Convert text to speech (TTS)
         if st.button("Convert to Speech"):
-            tts(text)
-
-    # Clear canvas button
-    if st.button("Clear Canvas"):
-        st.session_state.canvas_data = None
-        st.rerun()
+            language = select_language(language, languages)
+            tts(text, language)
 
 
-def tts(text):
+def select_language(language, languages):
+    # use the selected language
+    for key, value in languages.items():
+        if value == language:
+            return key
+
+
+def tts(text, language):
     # Implement your TTS model here
     st.write("Converting to speech:", text)
-    tts = gTTS(text)
+    tts = gTTS(text, lang=language)
     tts.save("output.mp3")
     st.audio("output.mp3")
